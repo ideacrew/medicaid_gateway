61e10700f3132afa35bef314abf2fc1d6a052804
Trey Evans <lewis.r.evans@gmail.com>
2021-04-28 14:50:27 -0400

    Add to ACES connectivity testing.

---
+++ app/controllers/aces/publishing_connectivity_tests_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Aces
+  # Controller for testing message publishing to ACES using the ATP protocol.
+  class PublishingConnectivityTestsController < ApplicationController
+    def new
+    end
+
+    def create
+      payload = params.require("payload")
+      @result = Aces::SubmitAccountTransferPayload.new.call(payload)
+      render "created"
+    end
+  end
+end
---
+++ app/operations/aces/publish_raw_payload.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+module Aces
+  # Takes a raw payload, encodes it, and invokes the ACES service with it.
+  class PublishRawPayload
+    send(:include, Dry::Monads[:result, :do])
+
+    # @param [String] raw_payload
+    # @return [Dry::Result]
+    def call(raw_payload)
+      built_request = yield build_request(raw_payload)
+      encoded_request = yield encode_request(built_request)
+      submit_request(encoded_request)
+    end
+
+    protected
+
+    def build_request(raw_payload)
+      Aces::BuildAccountTransferRequest.new.call(raw_payload)
+    end
+
+    def encode_request(request)
+      Aces::EncodeAccountTransferRequest.new.call(request)
+    end
+
+    def submit_request(encoded_request)
+      Aces::SubmitAccountTransferPayload.new.call(encoded_request)
+    end
+  end
+end
