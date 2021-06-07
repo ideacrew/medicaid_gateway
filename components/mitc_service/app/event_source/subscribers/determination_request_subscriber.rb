9236565e8fa92753439815d22e7468bebf04fd9e
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-07 14:57:43 -0400

    Revert "ref# integration: rubocop fix"
    This reverts commit 70df95811027eaa59043c92ef956e28ad3e1e830.
    
    revert integration changes

--- components/mitc_service/app/event_source/subscribers/determination_request_subscriber.rb
+++ components/mitc_service/app/event_source/subscribers/determination_request_subscriber.rb
@@ -2,19 +2,18 @@
 
 module EventSource
   module Subscribers
-    # Subscriber will receive response payload from mitc and perform validation along with persisting the payload
     class DeterminationRequestSubscriber
       include ::EventSource::Subscriber[http: '/determinations/eval']
 
       # # from: MagiMedicaidEngine of EA after Application's submission
       # # { event: magi_medicaid_application_submitted, payload: :magi_medicaid_application }
-      subscribe(:on_determinations_eval) do |_headers, _response|
+      subscribe(:on_determinations_eval) do |headers, response|
 
-        # TODO: requesst payload is not available
+        #TODO requesst payload is not available
         # correlation_id = JSON.parse(response.env.request_body)["Name"]
         # mitc_response_payload = response.merge(correlation_id: correlation_id)
 
-        # operation to persist
+        #operation to persist
         Medicaid::Application.new(mitc_response_payload).call
 
       end
--- config/initializers/event_source.rb
+++ config/initializers/event_source.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require 'event_source'
 
 EventSource.configure do |config|
