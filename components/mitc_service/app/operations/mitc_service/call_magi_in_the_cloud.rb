179ec0d970a37d4010ec64c8120b17a4aa3ce6de
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-07 12:12:57 -0400

    ref#integration: eventsource setup for medicaid gateway to mitc

--- components/mitc_service/app/operations/mitc_service/call_magi_in_the_cloud.rb
+++ components/mitc_service/app/operations/mitc_service/call_magi_in_the_cloud.rb
@@ -23,16 +23,14 @@ module MitcService
     private
 
     def call_magi_in_the_cloud(mitc_request_payload)
-      # TODO: Fix below URL
-      # TODO: Also we want to remove HTTParty and use Typheous.
-      # We want to add functionality exponentional backoff.
-      response = ::HTTParty.post('URL here',
-                                 :body => mitc_request_payload.to_json,
-                                 :headers => { 'Content-Type' => 'application/json',
-                                               'Accept' => 'application/json' })
-      # http://localhost:3000/determinations/eval
-      # JSON.parse(response.to_json ,:symbolize_names => true)
-      Success(response)
+      # TODO: refactor based on updates from event source
+      manager = EventSource::ConnectionManager.instance
+      connection = manager.connections_for(:http).first
+      channel = connection.channels[:'/determinations/eval']
+      publish_operation = channel.publish_operations[:'/determinations/eval']
+      publish_operation.call(mitc_request_payload.to_json)
+
+      Success("Successfully sent request payload to mitc")
     rescue StandardError => _e
       # TODO: Log the error
       if mitc_request_payload[:Name].present?
