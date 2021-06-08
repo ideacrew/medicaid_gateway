303ac00a6b4fe817ae66f6937c7beda3ea255baa
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-06-08 16:58:55 -0400

    Refs #Medicaid, add some documentation for Persistence Object fields

--- app/models/medicaid/aptc_household_member.rb
+++ app/models/medicaid/aptc_household_member.rb
@@ -7,12 +7,26 @@ module Medicaid
     include Mongoid::Document
     include Mongoid::Timestamps
 
+    # member_identifier is a unique identifier to identify a member
     field :member_identifier, type: String
+
+    # Computed household_count that is used for APTC/CSR determination
+    # Currently, the value will be 1 for each member
     field :household_count, type: Integer
+
+    # Computed annual_household_income_contribution by this member that is used for APTC/CSR determination
     field :annual_household_income_contribution, type: Float
+
+    # Tax Filer status of the member
     field :tax_filer_status, type: String
+
+    # Is this person applying?
     field :is_applicant, type: Boolean
+
+    # Premium amount that is used for this member for determining APTC/CSR
     field :benchmark_plan_monthly_premium_amount, type: Float
+
+    # Member Eligibility
     field :aptc_eligible, type: Boolean
     field :totally_ineligible, type: Boolean
     field :uqhp_eligible, type: Boolean
--- app/models/medicaid/benchmark_calculation_member.rb
+++ app/models/medicaid/benchmark_calculation_member.rb
@@ -6,8 +6,13 @@ module Medicaid
     include Mongoid::Document
     include Mongoid::Timestamps
 
+    # member_identifier is a unique identifier to identify a member
     field :member_identifier, type: String
+
+    # Relationship Kind
     field :relationship_kind_to_primary, type: String
+
+    # Premium amount that is used for this member for determining APTC/CSR
     field :member_premium, type: Float
 
     embedded_in :aptc_household, class_name: '::Medicaid::AptcHousehold'
