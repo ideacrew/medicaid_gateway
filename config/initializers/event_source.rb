e39b1090e2e3cf9816357c031b56463be8c461ae
Vishal Kalletla <vishuk1201@gmail.com>
2021-06-10 17:27:22 -0400

    move subscriber and api files to main app

--- config/initializers/event_source.rb
+++ config/initializers/event_source.rb
@@ -4,7 +4,7 @@ require 'event_source'
 
 EventSource.configure do |config|
   config.protocols = %w[amqp http]
-  config.pub_sub_root = Pathname.pwd.join('components', 'mitc_service', 'app', 'event_source')
+  config.pub_sub_root = Pathname.pwd.join('app', 'event_source')
   config.server_key = Rails.env.to_sym
 
   config.servers do |server|
@@ -17,7 +17,7 @@ EventSource.configure do |config|
   end
 end
 
-dir = Pathname.pwd.join('components', 'mitc_service', 'app', 'async_api_files')
+dir = Pathname.pwd.join('app', 'async_api_files')
 EventSource.async_api_schemas = ::Dir[::File.join(dir, '**', '*')].reject { |p| ::File.directory? p }.reduce([]) do |memo, file|
   memo << EventSource::AsyncApi::Operations::AsyncApiConf::LoadPath.new.call(path: file).success.to_h
 end
