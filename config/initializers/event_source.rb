84caefc68adab94628a2fad3a274ad170e115fd4
Raghuram Ganjala <raghuramg83@gmail.com>
2021-06-10 11:55:28 -0400

    Updated event source initializer

--- config/initializers/event_source.rb
+++ config/initializers/event_source.rb
@@ -5,7 +5,7 @@ EventSource.configure do |config|
   config.pub_sub_root =
     Pathname.pwd.join('spec', 'rails_app', 'app', 'event_source')
 
-  # config.environment = Rails.env
+  config.server_key = ENV['RAILS_ENV'] # production, development# Rails.env.to_sym
 
   config.servers do |server|
     # - RABBITMQ_HOST=""
@@ -14,21 +14,56 @@ EventSource.configure do |config|
     # - RABBITMQ_VERSION=""
     # - RABBITMQ_USERNAME=${RABBITMQ_USERNAME:-guest}
     # - RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD:-guest}
-    server.amqp do |rabbitmq|
-      rabbitmq.environment = :production
-      rabbitmq.host = ENV['RABBITMQ_HOST']
-      rabbitmq.vhost = ENV['RABBITMQ_VHOST']
-      rabbitmq.port = ENV['RABBITMQ_PORT']
-      rabbitmq.user_name = ENV['RABBITMQ_USERNAME']
-      rabbitmq.password = ENV['RABBITMQ_PASSWORD']
+    # server.amqp do |rabbitmq|
+    #   rabbitmq.environment = :production
+    #   rabbitmq.host = ENV['RABBITMQ_HOST']
+    #   rabbitmq.vhost = ENV['RABBITMQ_VHOST']
+    #   rabbitmq.port = ENV['RABBITMQ_PORT']
+    #   rabbitmq.user_name = ENV['RABBITMQ_USERNAME']
+    #   rabbitmq.password = ENV['RABBITMQ_PASSWORD']
 
+    #   # rabbitmq.url = "" # ENV['RABBITMQ_URL']
+    # end
+
+    # server.amqp do |rabbitmq|
+    #   rabbitmq.environment = :production
+    #   rabbitmq.host = 'amqp://localhost'
+    #   rabbitmq.vhost = '/event_source'
+    #   rabbitmq.port = '5672'
+    #   rabbitmq.url = ''
+    #   rabbitmq.user_name = 'guest'
+    #   rabbitmq.password = 'guest'
+    # end
+
+    server.amqp do |rabbitmq|
+      rabbitmq.host = ENV['RABBITMQ_HOST'] || "amqp://localhost"
+      STDERR.puts rabbitmq.host
+      rabbitmq.vhost = ENV['RABBITMQ_VHOST'] || "/event_source"
+      STDERR.puts rabbitmq.vhost
+      rabbitmq.port = ENV['RABBITMQ_PORT'] || "5672"
+      STDERR.puts rabbitmq.port
+      rabbitmq.url = ENV['RABBITMQ_URL'] || ""
+      STDERR.puts rabbitmq.url
+      rabbitmq.user_name = ENV['RABBITMQ_USERNAME'] || "guest"
+      STDERR.puts rabbitmq.user_name
+      rabbitmq.password = ENV['RABBITMQ_PASSWORD'] || "guest"
+      STDERR.puts rabbitmq.password
       # rabbitmq.url = "" # ENV['RABBITMQ_URL']
     end
   end
 
-  config.asyncapi_resources = [AcaEntities::AsyncApi::MedicaidGataway]
+  # config.asyncapi_resources = [AcaEntities::AsyncApi::MedicaidGataway]
   # config.asyncapi_resources = AcaEntities.find_resources_for(:enroll, %w[amqp resque_bus]) # will give you resouces in array of hashes form
   # AcaEntities::Operations::AsyncApi::FindResource.new.call(self)
+end
+
+
+dir = Rails.root.join('asyncapi')
+EventSource.async_api_schemas = ::Dir[::File.join(dir, '**', '*')].reject { |p| ::File.directory? p }.reduce([]) do |memo, file|
+  # read
+  # serialize yaml to hash
+  # Add to memo
+  memo << EventSource::AsyncApi::Operations::AsyncApiConf::LoadPath.new.call(path: file).success.to_h
 end
 
 EventSource.initialize!
