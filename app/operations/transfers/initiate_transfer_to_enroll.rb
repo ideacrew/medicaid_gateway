cce1d74ef20271be1600afe57fad5bdec1001727
Kristin Merbach <kristin@kristinmerbach.com>
2021-07-30 14:35:15 -0400

    atp to and from enroll

--- app/operations/transfers/initiate_transfer_to_enroll.rb
+++ app/operations/transfers/initiate_transfer_to_enroll.rb
@@ -13,13 +13,8 @@ module Transfers
     # @option opts [Hash] :fully_determined_medicaid_application
     # @option opts [String] :determined_aptc_eligible
     # @return [Dry::Monads::Result]
-<<<<<<< HEAD
     def call(params)
       event = yield build_event(params)
-=======
-    def call(params, event_name)
-      event = yield build_event(params, event_name)
->>>>>>> cc3190e (account transfers)
       result = send_to_enroll(event)
 
       Success(result)
@@ -27,7 +22,6 @@ module Transfers
 
     private
 
-<<<<<<< HEAD
     def build_event(params)
       puts "building event!"
       result = event("events.magi_medicaid.atp.enroll.transfer_in", attributes: params.to_h)
@@ -38,13 +32,6 @@ module Transfers
 
     def send_to_enroll(event)
       puts "sending event!"
-=======
-    def build_event(payload)
-      event('magi_medicaid.atp.enroll.transfer_in', attributes: payload)
-    end
-
-    def send_to_enroll(event)
->>>>>>> cc3190e (account transfers)
       event.publish
     end
   end
