40e8151a89e946b21aac07e371a1d6a9487280ea
Kristin Merbach <kristin@kristinmerbach.com>
2021-08-05 13:59:25 -0700

    outbound transforms

---
+++ spec/domain/operations/transfers/to_aces_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require 'aca_entities/serializers/xml/medicaid/atp'
+require 'aca_entities/medicaid/atp'
+require 'aca_entities/atp/operations/aces/generate_xml.rb'
+
+describe Transfers::ToAces, "given an ATP valid payload, transfer it to Aces" do
+  include Dry::Monads[:result, :do]
+
+  let(:atp_hash) {File.read("./spec/test_data/application_and_family.json")}
+
+  let(:transfer) {described_class.new}
+
+  context 'success' do
+    context 'with valid application' do
+      before do
+        @result = transfer.call(atp_hash)
+      end
+
+      it 'should return success' do
+        expect(@result).to be_success
+      end
+
+      it 'should return success with message' do
+        expect(@result.success).to eq('Successfully transferred in account')
+      end
+    end
+  end
+
+end
