5c56e39d32210393bf1d841ce32aac410f50e4c5
Kristin Merbach <kristin@kristinmerbach.com>
2021-08-23 10:22:45 -0400

    resource registry

--- app/operations/transfers/to_aces.rb
+++ app/operations/transfers/to_aces.rb
@@ -2,6 +2,9 @@
 
 require 'dry/monads'
 require 'dry/monads/do'
+require 'aca_entities/serializers/xml/medicaid/atp'
+require 'aca_entities/medicaid/atp'
+require 'aca_entities/atp/operations/aces/generate_xml'
 
 module Transfers
   # Transfers and family and account from Enroll to a Medicaid service
@@ -14,33 +17,39 @@ module Transfers
     def call(params, service)
       xml =      yield create_transfer_request(params)
       validated  = yield schema_validation(xml)
-      # validated  = yield business_validation(validated)
+      validated  = yield business_validation(validated)
       initiate_transfer(validated, service)
     end
 
     private
 
     def create_transfer_request(params)
+      puts "created transfer"
       transfer_request = AcaEntities::Atp::Operations::Aces::GenerateXml.new.call(params)
       Success(transfer_request)
     end
 
     def schema_validation(xml)
+      puts "validated transfer"
       result = Transfers::ValidateTransferXml.new.call(xml.value!)
       result.success? ? Success(xml) : Failure(result)
     end
 
     def business_validation(xml)
+      puts "validating against schematron"
       result = Transfers::ExecuteBusinessXmlValidations.new.call(xml.value!)
+      puts result.inspect
       result.success? ? Success(xml) : Failure(result)
     end
 
     def initiate_transfer(payload, service)
+      puts "initiate_transfer"
       if service == "aces"
         Aces::PublishRawPayload.new.call(payload)
       else
         Curam::PublishRawPayload.new.call(payload)
       end
+      puts "transferred"
     end
   end
 end
