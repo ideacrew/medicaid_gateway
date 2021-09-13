3690793b688b155ebe8fd65bb1123e6c738589b9
Kristin Merbach <kristin@kristinmerbach.com>
2021-09-13 14:05:00 -0400

    more rubocop errors

--- app/operations/curam/encode_account_transfer_check_request.rb
+++ app/operations/curam/encode_account_transfer_check_request.rb
@@ -46,12 +46,12 @@ module Curam
                                  "xmlns:v1" => "http://xmlns.dhs.dc.gov/dcas/esb/acctransappstatuccheck/V1"
                                }) do |envelope|
           encode_soap_header(envelope, request)
-          xml[:soapenv].Body do |body|
-            xml[:v1].AccTransStatusByIdReq do |req|
+          xml[:soapenv].Body {
+            xml[:v1].AccTransStatusByIdReq {
               xml[:v1].GLOBALAPPLICATIONID request.global_application_id
               xml[:v1].LASTWRITTEN request.last_written
-            end
-          end
+            }
+          }
         end
       end
       Success(builder.to_xml)
