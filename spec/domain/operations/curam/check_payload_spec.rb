9578bf081872e358132499aa10f3ccc09eeaa93c
Kristin Merbach <kristin@kristinmerbach.com>
2021-09-16 09:26:48 -0400

    fixing check payload spec to be more robust

--- spec/domain/operations/curam/check_payload_spec.rb
+++ spec/domain/operations/curam/check_payload_spec.rb
@@ -18,10 +18,10 @@ describe Curam::CheckPayload, "given a valid account transfer check request", db
   let(:response_body) do
     "<env:Envelope>
       <env:Body>
-        <AccTransStatusByIdRes>
+        <AccTransStatusByIdRes xmlns:tns=\"http://xmlns.dhs.dc.gov/dcas/esb/acctransappstatuccheck/V1\">
           <tns:AccTransStatusResData>
             <tns:APPLICATIONID>6918320676013080576</tns:APPLICATIONID>
-            <tns:GLOBALAPPLICATIONID>SBM1624211750508034</tns:GLOBALAPPLICATIONID>
+            <tns:GLOBALAPPLICATIONID>SBM123</tns:GLOBALAPPLICATIONID>
             <tns:STATUS>Federal Exchange Inbound Error</tns:STATUS>
             <tns:VERSIONNO>6</tns:VERSIONNO>
             <tns:LASTWRITTEN>2021-01-01T08:55:01.000-04:00</tns:LASTWRITTEN>
@@ -46,12 +46,17 @@ describe Curam::CheckPayload, "given a valid account transfer check request", db
     allow(MedicaidGatewayRegistry[:curam_connection].setting(:curam_atp_service_password)).to receive(:item).and_return("random_pw")
     allow(MedicaidGatewayRegistry[:curam_connection].setting(:curam_check_service_uri)).to receive(:item).and_return("random_uri")
     allow(operation).to receive(:submit_check).and_return(event)
-    operation.call(id)
   end
 
-  it "should update the transfer callback status" do
+  it "initally the transfer should not have a callback status" do
     updated_transfer = Aces::Transfer.where(application_identifier: id).first
-    expect(updated_transfer.callback_status).to eq "N/A"
+    expect(updated_transfer.callback_status).to_not eq "N/A"
+  end
+
+  it "calling the check payload should update the transfer callback status" do
+    operation.call(id)
+    updated_transfer = Aces::Transfer.where(application_identifier: id).first
+    expect(updated_transfer.callback_status).to eq "Federal Exchange Inbound Error"
   end
 
 end
