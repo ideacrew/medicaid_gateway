bb100d23b557ac7dffed24e2f6732ea0a93e2863
mdkaraman <mdkaramanov@gmail.com>
2021-09-17 15:02:28 -0400

    continue adding mec_check

--- app/operations/aces/initiate_mec_check.rb
+++ app/operations/aces/initiate_mec_check.rb
@@ -13,7 +13,7 @@ module Aces
     def call(application_payload)
       people = get_people(application_payload)
       checks = mec_check(people)
-      results = save_check(checks)
+      results = save_check(checks, application_payload)
       publish_to_enroll(checks)
     end
 
@@ -29,14 +29,28 @@ module Aces
       people.each do |person|
         person_hash = {}
         person_hash["person"] = {"person": person} 
-        result = Aces::MecCheck.new.call(person_hash)
-        results[person["hbx_id"]] = result
+        result = Aces::MecCheckCall.new.call(person_hash)
+        results[person["hbx_id"]] = result.value!.to_json
       end
-      puts results
+      results
     end
 
-    def save_check(check_results)
+    def save_check(check_results, application_payload)
       # TO DO save the data - need to decide if per person or per applications
+      payload_json = JSON.parse(application_payload)
+      application = payload_json["family"]["magi_medicaid_applications"][0]["hbx_id"]
+      family = payload_json["family"]["magi_medicaid_applications"][0]["family_reference"]["hbx_id"]
+      responses = check_results
+      puts "application identifier: #{application}"
+      puts "family_identifier: #{family}"
+      puts "applicant_responses: #{responses}"
+      Aces::CreateMecCheck.new.call(
+        {
+          application_identifier: application,
+          family_identifier: family,
+          applicant_responses: responses
+        }
+      )
     end
 
     def publish_to_enroll
