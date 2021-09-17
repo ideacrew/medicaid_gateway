1b7238048893918e0b57b9cd2d0ac84420196524
Kristin Merbach <kristin@kristinmerbach.com>
2021-09-17 11:57:55 -0400

    started building out the mec_check process

---
+++ app/models/aces/mec_check.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Aces
+  # MecCheck to see if person is in Medicaid System
+  class MecCheck
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    # Unique Identifier(hbx_id) of the application.
+    # For example: EA's FinancialAssistance::Application's hbx_id
+    field :application_identifier, type: String
+
+    # Unique Identifier(hbx_id) of the family.
+    # For example: EA's FinancialAssistance::Family's hbx_id
+    field :family_identifier, type: String
+
+    # Server response from the service
+    field :applicant_responses, type: Hash
+
+  end
+end
