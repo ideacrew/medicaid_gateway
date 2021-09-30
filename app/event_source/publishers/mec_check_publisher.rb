2700900a768eaeb0add38773cc9e5c0f4d612c7e
Michael Karamanov <michael.karamanov@ideacrew.com>
2021-09-30 17:59:21 -0400

    Revert "Mec check 21.10.11 (#33)" (#34)
    This reverts commit 5af1ec0a2f950f14c98f47e55bafaf8760baf4d8.

--- app/event_source/publishers/mec_check_publisher.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Publishers
-  # Publisher will send MEC check results payload to EA
-  class MecCheckPublisher
-    include ::EventSource::Publisher[amqp: 'magi_medicaid.mec_check']
-    register_event 'mec_checked'
-  end
-end
