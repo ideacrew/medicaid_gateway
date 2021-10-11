a7640be6b55de127bbd33aca786e8389bde97f25
Kristin Merbach <kristin@kristinmerbach.com>
2021-10-11 09:53:56 -0400

    event date picker fix and using successful methos on view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,8 +4,10 @@
 class ReportsController < ApplicationController
 
   def events
-    @start_on = session[:start] || Date.today
-    @end_on = session[:end] || Date.today
+    start_on = Date.strptime(params.fetch(:start_on), "%m/%d/%Y") if params.key?(:start_on)
+    @start_on = start_on || session[:start] || Date.today
+    end_on = Date.strptime(params.fetch(:end_on), "%m/%d/%Y") if params.key?(:end_on)
+    @end_on = end_on || session[:end] || Date.today
     range = @start_on.beginning_of_day..@end_on.end_of_day
 
     applications = Medicaid::Application.where(created_at: range).or(updated_at: range)
