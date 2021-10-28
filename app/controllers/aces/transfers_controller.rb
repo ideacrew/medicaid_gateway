e458cda14b039a8a26d560b63dceb9f5d8b116b8
Kristin Merbach <kristin@kristinmerbach.com>
2021-10-28 09:12:37 -0400

    specify authentication

--- app/controllers/aces/transfers_controller.rb
+++ app/controllers/aces/transfers_controller.rb
@@ -3,6 +3,7 @@
 module Aces
   # Transfers from Enroll to outside agency
   class TransfersController < ActionController::Base
+    before_action :authenticate_user!
     def show
       @transfer = Aces::Transfer.find(params[:id])
       @failure_status = @transfer.failure.nil?
