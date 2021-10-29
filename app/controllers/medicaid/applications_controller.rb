80d3ddedd84019ffe6705bba3812edd554c20120
Kristin Merbach <kristin@kristinmerbach.com>
2021-10-29 14:46:25 -0400

    rubocop

--- app/controllers/medicaid/applications_controller.rb
+++ app/controllers/medicaid/applications_controller.rb
@@ -5,22 +5,20 @@ module Medicaid
   class ApplicationsController < ActionController::Base
     def show
       @application = Medicaid::Application.find(params[:id])
-      @application_request_payload = is_json(@application.application_request_payload)
-      @application_response_payload = is_json(@application.application_response_payload)
-      @medicaid_request_payload = is_json(@application.medicaid_request_payload)
-      @medicaid_response_payload = is_json(@application.medicaid_response_payload)
-      @other_factors = is_json(@application.other_factors)
+      @application_request_payload = parse_json(@application.application_request_payload)
+      @application_response_payload = parse_json(@application.application_response_payload)
+      @medicaid_request_payload = parse_json(@application.medicaid_request_payload)
+      @medicaid_response_payload = parse_json(@application.medicaid_response_payload)
+      @other_factors = parse_json(@application.other_factors)
       render layout: "application"
     end
 
     private
 
-    def is_json(value)
-      begin
-        JSON.parse(value)
-      rescue Exception => e
-        value
-      end
+    def parse_json(value)
+      JSON.parse(value)
+    rescue JSON::ParserError
+      value
     end
 
   end
