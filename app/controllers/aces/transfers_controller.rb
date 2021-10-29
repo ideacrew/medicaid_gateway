44ad6fea6fbaf9e545ee54cd208584af0e2c099a
Kristin Merbach <kristin@kristinmerbach.com>
2021-10-29 14:38:28 -0400

    switch authentication exclude soap ones

--- app/controllers/aces/transfers_controller.rb
+++ app/controllers/aces/transfers_controller.rb
@@ -3,7 +3,6 @@
 module Aces
   # Transfers from Enroll to outside agency
   class TransfersController < ActionController::Base
-    before_action :authenticate_user!
     def show
       @transfer = Aces::Transfer.find(params[:id])
       @failure_status = @transfer.failure.nil?
--- app/controllers/medicaid/applications_controller.rb
+++ app/controllers/medicaid/applications_controller.rb
@@ -5,7 +5,23 @@ module Medicaid
   class ApplicationsController < ActionController::Base
     def show
       @application = Medicaid::Application.find(params[:id])
+      @application_request_payload = is_json(@application.application_request_payload)
+      @application_response_payload = is_json(@application.application_response_payload)
+      @medicaid_request_payload = is_json(@application.medicaid_request_payload)
+      @medicaid_response_payload = is_json(@application.medicaid_response_payload)
+      @other_factors = is_json(@application.other_factors)
       render layout: "application"
     end
+
+    private
+
+    def is_json(value)
+      begin
+        JSON.parse(value)
+      rescue Exception => e
+        value
+      end
+    end
+
   end
 end
