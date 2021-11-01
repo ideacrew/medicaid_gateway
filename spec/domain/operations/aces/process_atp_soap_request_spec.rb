7aab2f0950b5901deadb1a2bea31d5d694ffdc5d
mdkaraman <mdkaramanov@gmail.com>
2021-11-01 14:52:21 -0400

    stub transfer service

--- spec/domain/operations/aces/process_atp_soap_request_spec.rb
+++ spec/domain/operations/aces/process_atp_soap_request_spec.rb
@@ -16,10 +16,12 @@ describe Aces::ProcessAtpSoapRequest, "given a soap envelope with an valid xml p
   before :each do
     allow(MedicaidGatewayRegistry).to receive(:[]).with(:aces_connection).and_return(feature_ns)
     allow(MedicaidGatewayRegistry).to receive(:[]).with(:transfer_to_enroll)
+    allow(MedicaidGatewayRegistry).to receive(:[]).with(:transfer_service)
     allow(MedicaidGatewayRegistry[:aces_connection]).to receive(:setting).with(:aces_atp_caller_username).and_return(double)
     allow(MedicaidGatewayRegistry[:aces_connection].setting(:aces_atp_caller_username)).to receive(:item).and_return("SOME_SOAP_USER")
     allow(MedicaidGatewayRegistry[:aces_connection]).to receive(:setting).with(:aces_atp_caller_password).and_return(double)
     allow(MedicaidGatewayRegistry[:aces_connection].setting(:aces_atp_caller_password)).to receive(:item).and_return("SOME SOAP PASSWORD")
+    allow(MedicaidGatewayRegistry[:transfer_service]).to receive(:item).and_return("SERVICE")
     allow(MedicaidGatewayRegistry).to receive(:feature_enabled?).with(:transfer_to_enroll).and_return(false)
     allow(MedicaidGatewayRegistry).to receive(:feature_enabled?).with(:resubmit_to_enroll).and_return(false)
   end
@@ -29,6 +31,7 @@ describe Aces::ProcessAtpSoapRequest, "given a soap envelope with an valid xml p
       @transfer = create :inbound_transfer
       @result = operation.call(xml, @transfer.id)
     end
+
     it "payload processing should be successful" do
       expect(@result.success?).to be_truthy
     end
