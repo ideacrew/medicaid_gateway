482eb0b2dcd72619abaf50334ef240f1fbdf93ea
Michael Karamanov <michael.karamanov@ideacrew.com>
2022-01-10 16:52:55 -0500

    configure session store in production (#126)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,11 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.session_store :cache_store,
+                       key: "_session",
+                       compress: true,
+                       pool_size: 5,
+                       expire_after: 1.year
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
