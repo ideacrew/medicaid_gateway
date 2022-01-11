0c3d6c88aa3a0ffe8b3d8bee3fdefd0ab794f5d4
mdkaraman <mdkaramanov@gmail.com>
2022-01-11 13:03:07 -0500

    remove error logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
-# Application Controller
 class ApplicationController < ActionController::Base
-  before_action :manual_authenticate_user
-
-  def manual_authenticate_user
-    authenticate_user!
-  rescue StandardError => e
-    puts "manual_authenticate_user"
-    puts e.backtrace.join("\n")
-    raise e
-  end
+  before_action :authenticate_user!
 end
