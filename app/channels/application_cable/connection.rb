553b367fb58f5155401de8e93e9ec7b72ea300f5
Michael Karamanov <michael.karamanov@ideacrew.com>
2022-01-13 12:16:08 -0500

    authenticate user on routes (#137)
    Co-authored-by: Bill Transue <transue@gmail.com>

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -16,7 +16,7 @@ module ApplicationCable
     protected
 
     def find_verified_user
-      self.current_user = env["warden"].user(:user) || reject_unauthorized_connection
+      env["warden"].user || reject_unauthorized_connection
     end
   end
 end
