d5b19fed1ddc3fd0c053910748b2a8ccd191d2f3
Kristin Merbach <kristin@kristinmerbach.com>
2022-01-14 11:22:01 -0500

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@
 
 class ApplicationController < ActionController::Base
   # skip authentication on reflexes
-  before_action :authenticate_user!, :unless => proc { !!@stimulus_reflex }
+  before_action :authenticate_user!, :if => proc { !@stimulus_reflex }
 end
