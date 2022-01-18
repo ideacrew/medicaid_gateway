26ccdd50d6ca4d7a44e57f5ec1528730fd5afc7a
Kristin Merbach <kristin@kristinmerbach.com>
2022-01-18 14:36:46 -0500

    rubocop

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -87,7 +87,7 @@ class ReportsController < ApplicationController
   end
 
   def applications
-    Medicaid::Application.where(created_at: range).or(updated_at: range).only(:application_identifier, :created_at, :_id, :application_response_payload)
+    Medicaid::Application.where(created_at: range).or(updated_at: range).only(:application_identifier, :created_at, :application_response_payload)
   end
 
   def transfers
