46931ef2234af8d687bcb93cf9dbaef1dc9339c8
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2022-01-20 15:12:05 -0500

    session switch (#149)
    * update indexing
    
    * update indexing
    
    * declare sessions new way and switch the keys for different reports to be uniq
    
    * rubocop

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,13 +4,18 @@ module ApplicationCable
 
   # stimulus reflex connection differentiator
   class Connection < ActionCable::Connection::Base
-    identified_by :session_id
-    # identified_by :current_user
+    # identified_by :session_id
+    identified_by :current_user
 
     def connect
       # self.current_user = find_verified_user
-      self.session_id = request.session.id
-      reject_unauthorized_connection unless session_id
+      # self.session_id = request.session.id
+      # reject_unauthorized_connection unless session_id
+      user_id = cookies.encrypted[:user_id]
+      return reject_unauthorized_connection if user_id.nil?
+      user = User.find(user_id)
+      return reject_unauthorized_connection if user.nil?
+      self.current_user = user
     end
 
     protected
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+# Application controller
 class ApplicationController < ActionController::Base
   # skip authentication on reflexes
   before_action :authenticate_user!, :if => proc { !@stimulus_reflex }
+  before_action :set_action_cable_identifier
+
+  def set_action_cable_identifier
+    cookies.encrypted[:user_id] = current_user&.id
+  end
 end
