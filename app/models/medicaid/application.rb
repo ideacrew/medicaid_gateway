3e1732708696582e243c17e367b9d26689423b7a
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2022-01-20 13:53:47 -0500

    update indexing (#148)
    * update indexing
    
    * update indexing

--- app/models/medicaid/application.rb
+++ app/models/medicaid/application.rb
@@ -29,9 +29,8 @@ module Medicaid
     embeds_many :aptc_households, class_name: '::Medicaid::AptcHousehold'
     accepts_nested_attributes_for :aptc_households
 
-    index({ created_at: -1, updated_at: -1 })
-    index({ created_at: 1 })
-    index({ application_identifier: 1 })
+    index({ created_at: 1, updated_at: 1 })
+    index({ updated_at: 1 })
 
     def successful?
       return true unless application_response_payload.blank?
