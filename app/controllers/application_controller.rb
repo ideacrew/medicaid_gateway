ff785dedcfb45612a3cd7d413b517393f08eb750
Kristin Merbach <kristin@kristinmerbach.com>
2022-01-20 15:01:55 -0500

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Application controller
 class ApplicationController < ActionController::Base
   # skip authentication on reflexes
   before_action :authenticate_user!, :if => proc { !@stimulus_reflex }
