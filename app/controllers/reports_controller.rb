1fc89f8cfb056b1fc901bb870defbb6c79c4dfab
Kristin Merbach <kristin@kristinmerbach.com>
2022-02-04 16:51:27 -0500

    report controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,8 +21,8 @@ class ReportsController < ApplicationController
   end
 
   def medicaid_application_check
-    @start_on = start_on || session[:ma_start] || Date.today
-    @end_on = end_on || session[:ma_end] || Date.today
+    @start_on = start_on || session_date(session[:ma_start]) || Date.today
+    @end_on = end_on || session_date(session[:ma_end]) || Date.today
     @application_ids = Medicaid::Application.all.distinct(:application_identifier).uniq.sort
     application_id = params.fetch(:app) if params.key?(:app)
     @app = application_id || session[:app]
