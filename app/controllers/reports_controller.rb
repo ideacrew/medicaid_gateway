fb7af21211f8f3a37797b7098477614786e06915
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2022-04-06 11:43:37 -0400

    add caching to determinations (#191)
    * add index and go directly to result by id
    
    * add more indexes
    
    * cache all applications
    
    * rubocop
    
    * add safe operators

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -24,11 +24,10 @@ class ReportsController < ApplicationController
     authorize :user, :determinations?
     @start_on = start_on || session_date(session[:ma_start]) || Date.today
     @end_on = end_on || session_date(session[:ma_end]) || Date.today
-    @application_ids = Medicaid::Application.all.only(:application_identifier, :id).sort
     application_id = params.fetch(:app) if params.key?(:app)
     @app = application_id || session[:app]
     @applications = if @app
-                      Medicaid::Application.where(application_identifier: @app).page params[:page]
+                      @all_applications.where(application_identifier: @app).page params[:page]
                     else
                       applications.order(updated_at: :desc).page params[:page]
                     end
@@ -161,8 +160,12 @@ class ReportsController < ApplicationController
   end
 
   def applications
-    Medicaid::Application.only(:application_identifier, :created_at, :application_response_payload, :medicaid_response_payload)
-                         .where(created_at: range).or(updated_at: range)
+    last_updated = Medicaid::Application.all.distinct(:updated_at).max&.utc&.to_s(:number)
+    cache_key = "applications/#{last_updated}"
+    @all_applications = Rails.cache.fetch(cache_key) do
+      Medicaid::Application.all.only(:application_identifier, :created_at, :application_response_payload, :medicaid_response_payload)
+    end
+    @all_applications.where(created_at: range).or(updated_at: range)
   end
 
   def daily_report_applications
