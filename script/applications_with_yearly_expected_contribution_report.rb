6664e23f234d4f5775bf5b677dfc961785ad1da5
Sai Kumar Koptagiri <saikumarkotagiri9@gmail.com>
2022-09-16 16:20:04 -0400

    change logic and add logging

--- script/applications_with_yearly_expected_contribution_report.rb
+++ script/applications_with_yearly_expected_contribution_report.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 # This script generates a CSV report with information about Applications with YearlyExpectedContribution
-# rails runner script/applications_with_yearly_expected_contribution_report.rb -e production
+# bundle exec rails runner script/applications_with_yearly_expected_contribution_report.rb -e production
 
+start_time = DateTime.current
+puts "ApplicationsWithYearlyExpectedContributionReport start_time: #{start_time}"
 field_names = %w[ApplicationHbxID AptcHouseholdsWithYearlyExpectedContribution]
 file_name = "#{Rails.root}/applications_with_yearly_expected_contributions_for_aptc_households.csv"
 source_file = "#{Rails.root}/list_of_latest_determined_2022_applications_per_family.csv"
@@ -23,12 +25,12 @@ CSV.open(file_name, 'w', force_quotes: true) do |csv|
     thh_hbx_assigned_id_with_expected_contribution = thhs_information.inject({}) do |thhs_hash, thhs_info|
       thh_hbx_assigned_id, person_hbx_ids = thhs_info
       aptc_household = application.aptc_households.detect do |aptc_hh|
-        (aptc_hh.aptc_household_members.map(&:member_identifier) & person_hbx_ids).present?
+        aptc_hh.total_expected_contribution_amount.present? &&
+          !aptc_hh.total_expected_contribution_amount.zero? &&
+          (aptc_hh.aptc_household_members.map(&:member_identifier) & person_hbx_ids).present?
       end
 
-      if aptc_household&.total_expected_contribution_amount.present?
-        thhs_hash[thh_hbx_assigned_id] = aptc_household.total_expected_contribution_amount
-      end
+      thhs_hash[thh_hbx_assigned_id] = aptc_household.total_expected_contribution_amount if aptc_household.present?
       thhs_hash
     end
 
@@ -39,3 +41,5 @@ CSV.open(file_name, 'w', force_quotes: true) do |csv|
     puts "Unable to process ApplicationHbxID: #{row['ApplicationHbxID']}, message: #{e.message}, backtrace: #{e.backtrace}"
   end
 end
+total_time_in_mins = ((DateTime.current - start_time) * 24 * 60).to_f.ceil
+puts "ApplicationsWithYearlyExpectedContributionReport end_time: #{end_time}, total_time_taken_in_minutes: #{total_time_in_mins}"
