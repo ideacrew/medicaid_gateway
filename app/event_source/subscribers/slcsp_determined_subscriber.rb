079a8b9547b3253c1330b77ee1653498ca02c622
Sai Kumar Koptagiri <saikumarkotagiri9@gmail.com>
2022-10-05 19:15:05 -0400

    add additional operations that are needed.

--- app/event_source/subscribers/slcsp_determined_subscriber.rb
+++ app/event_source/subscribers/slcsp_determined_subscriber.rb
@@ -8,18 +8,19 @@ module Subscribers
     # subscribe(:on_enroll_iap_benchmark_products) do |delivery_info, _metadata, response|
     subscribe(:on_slcsp_determined) do |delivery_info, _metadata, response|
       payload = JSON.parse(response, :symbolize_names => true)
-      logger.debug "on_enroll_iap_benchmark_products: payload: #{payload}"
-      # result = ::Eligibilities::Medicaid::RequestDetermination.new.call(payload)
+      logger.info "on_slcsp_determined: payload: #{payload}"
+      result = ::ProcessSubscriberResponses::SlcspDetermined.new.call(payload)
 
       if result.success?
-        logger.debug "on_enroll_iap_benchmark_products: acked"
+        message = result.success
+        logger.info "on_slcsp_determined: acked #{message.is_a?(Hash) ? message[:event] : message}"
       else
         errors = result.failure.errors.to_h
-        logger.debug "on_enroll_iap_benchmark_products: acked (nacked) due to:#{errors}"
+        logger.info "on_slcsp_determined: acked (nacked) due to:#{errors}"
       end
       ack(delivery_info.delivery_tag)
     rescue StandardError, SystemStackError => e
-      logger.debug "on_enroll_iap_benchmark_products: error backtrace: #{e.backtrace}; acked (nacked)"
+      logger.info "on_slcsp_determined: error: #{e} backtrace: #{e.backtrace}; acked (nacked)"
       ack(delivery_info.delivery_tag)
     end
   end
