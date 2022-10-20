c118dc8136421344fab7d34cd3ac60417edac7d3
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2022-10-20 14:16:42 -0400

    Publish determinations for renewal applications with Dynamic SLCSP (#281)
    * Publish determinations for renewal applications with Dynamic SLCSP
    
    * typo

--- spec/factories/applications.rb
+++ spec/factories/applications.rb
@@ -9,6 +9,7 @@ FactoryBot.define do
     dynamic_slcsp_request_payload { "{\"us_state\":\"DC\",\"hbx_id\":\"200000123\"}" }
     dynamic_slcsp_response_payload { "{\"us_state\":\"DC\",\"hbx_id\":\"200000123\"}" }
     medicaid_response_payload { "{\"Applicants\":[{\"Person ID\":95}]}" }
+    is_renewal { false }
 
     trait :with_aptc_households do
       aptc_households do
