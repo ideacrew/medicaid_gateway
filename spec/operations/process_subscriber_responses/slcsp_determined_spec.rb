bc1bec7a6b10ceaf0adb64ee02e82acda3c5ba79
Sai Kumar Koptagiri <saikumarkotagiri9@gmail.com>
2022-10-20 13:42:30 -0400

    typo

--- spec/operations/process_subscriber_responses/slcsp_determined_spec.rb
+++ spec/operations/process_subscriber_responses/slcsp_determined_spec.rb
@@ -65,13 +65,13 @@ RSpec.describe ProcessSubscriberResponses::SlcspDetermined, dbclean: :after_each
         aptc_hh.update_attributes!(tax_household_identifier: '12345')
       end
 
-      context do 'regular application'
+      context 'regular application' do
         it 'should return success' do
           expect(subject.success?).to be_truthy
         end
       end
 
-      context do 'renewal application'
+      context 'renewal application' do
         it 'should return success' do
           application.update_attributes!(is_renewal: true)
           expect(subject.success?).to be_truthy
