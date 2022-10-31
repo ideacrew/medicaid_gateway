5d0b6baaa6f3aea10b3f1e1f70bc52059575f0b0
mdkaraman <mdkaramanov@gmail.com>
2022-10-31 13:25:43 -0400

    update build and encode operations

--- app/operations/aces/applicant_mec_check_call.rb
+++ app/operations/aces/applicant_mec_check_call.rb
@@ -78,7 +78,7 @@ module Aces
       else
         transfer = transfer.gsub("xmlns:v1=\"http://xmlns.dhcf.dc.gov/dcas/Medicaid/Eligibility/xsd/v1\"", "")
         transfer = transfer.gsub("xmlns:v11=\"http://xmlns.dc.gov/dcas/common/CommonNative/xsd/V1\"", "")
-        Curam::BuildAccountTransferRequest.new.call(transfer)
+        Curam::MecCheck::BuildMecCheckRequest.new.call(transfer)
       end
     end
 
@@ -86,7 +86,7 @@ module Aces
       if MedicaidGatewayRegistry[:transfer_service].item == "aces"
         Aces::EncodeAccountTransferRequest.new.call(payload)
       else
-        Curam::EncodeAccountTransferRequest.new.call(payload)
+        Curam::Atp::EncodeMecCheckRequest.new.call(payload)
       end
     end
 
