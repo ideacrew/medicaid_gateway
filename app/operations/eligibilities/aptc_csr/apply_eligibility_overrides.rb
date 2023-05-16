ac0db2406d6b340d366236036e54c7833922ae53
Esme Rubinstein <erubinst@oberlin.edu>
2023-05-16 12:11:56 -0400

    updating specs

--- app/operations/eligibilities/aptc_csr/apply_eligibility_overrides.rb
+++ app/operations/eligibilities/aptc_csr/apply_eligibility_overrides.rb
@@ -20,6 +20,7 @@ module Eligibilities
         mm_app_hash = @mm_application.to_h
         mm_app_hash[:tax_households].each do |mm_thh|
           mm_thh[:tax_household_members].each do |thhm|
+            binding.irb
             if only_medicaid_ineligible_due_to_immigration?(thhm) && (pregnancy_override?(thhm) || nineteen_to_twenty_one_override?(thhm))
               thhm[:product_eligibility_determination][:is_magi_medicaid] = true
             elsif only_chip_ineligible_due_to_immigration?(thhm) && under_eighteen_chip_override?(thhm)
@@ -31,7 +32,7 @@ module Eligibilities
       end
 
       def pregnancy_override?(thhm)
-        is_pregnant = applicant_by_reference(thhm.applicant_reference.person_hbx_id)&.pregnancy_information&.is_pregnant
+        is_pregnant = applicant_by_reference(thhm[:applicant_reference][:person_hbx_id])&.pregnancy_information&.is_pregnant
         is_pregnant && MedicaidGatewayRegistry[:eligibility_override].settings(:mitc_override_not_lawfully_present_pregnant).item
       end
 
@@ -42,11 +43,11 @@ module Eligibilities
       end
 
       def under_eighteen_chip_override?(thhm)
-        age(thhm) < 19 && MedicaidRegistry[:eligibility_override].settings(:mitc_override_not_lawfully_present_chip_eligible).item
+        age(thhm) < 19 && MedicaidGatewayRegistry[:eligibility_override].settings(:mitc_override_not_lawfully_present_chip_eligible).item
       end
 
       def age(thhm)
-        dob = thhm.applicant_reference.dob
+        dob = thhm[:applicant_reference][:dob]
         ((Time.zone.now - dob.to_time) / 1.year.seconds).floor
       end
 
--- spec/operations/eligibilities/aptc_csr/apply_eligibility_overrides_spec.rb
+++ spec/operations/eligibilities/aptc_csr/apply_eligibility_overrides_spec.rb
@@ -19,10 +19,26 @@ describe Eligibilities::AptcCsr::ApplyEligibilityOverrides do
         is_lawful_presence_self_attested: false }
     end
 
+    let(:product_eligibility_determination2) do
+      { is_ia_eligible: false,
+        is_medicaid_chip_eligible: false,
+        is_non_magi_medicaid_eligible: false,
+        is_totally_ineligible: false,
+        is_without_assistance: false,
+        is_magi_medicaid: false,
+        magi_medicaid_monthly_household_income: 6474.42,
+        medicaid_household_size: 1,
+        magi_medicaid_monthly_income_limit: 3760.67,
+        magi_as_percentage_of_fpl: 10.0,
+        magi_medicaid_category: "parent_caretaker",
+        magi_medicaid_ineligibility_reasons: ["Applicant did not meet citizenship/immigration requirements"] }
+    end
+
     let(:input_application) do
       app_params = mm_application_entity.to_h
       app_params[:applicants].first[:pregnancy_information] = pregnancy_information2
       app_params[:applicants].first[:citizenship_immigration_status_information] = citizenship_immigration_status_information2
+      app_params[:tax_households].first[:tax_household_members].first[:product_eligibility_determination] = product_eligibility_determination2
       ::AcaEntities::MagiMedicaid::Operations::InitializeApplication.new.call(app_params).success
     end
 
