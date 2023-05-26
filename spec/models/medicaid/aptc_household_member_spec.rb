6869f25bed6d2d9ee0276f5affdf63fd7ca7e8ad
Sarah Austin <scaustin34@gmail.com>
2023-05-26 15:40:07 -0400

    removed unnecessary spec

--- spec/models/medicaid/aptc_household_member_spec.rb
+++ spec/models/medicaid/aptc_household_member_spec.rb
@@ -3,31 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe ::Medicaid::AptcHouseholdMember, type: :model, dbclean: :after_each do
-  context 'embeds_many :member_determinations' do
-    before do
-      @association = described_class.reflect_on_association(:member_determinations)
-    end
-
-    it "should return association's class as EmbedsMany" do
-      expect(@association.class).to eq(Mongoid::Association::Embedded::EmbedsMany)
-    end
-
-    it "should return association's name as member_determinations" do
-      expect(@association.name).to eq(:member_determinations)
-    end
-  end
-
-  context 'valid params' do
+  context 'with valid params' do
     let(:application) { FactoryBot.create(:application, :with_aptc_households) }
 
     before do
       application.save!
       @app = application
-    end
-
-    it 'should be saved correctly' do
-      member = Medicaid::Application.find(@app.id).aptc_households.first.aptc_household_members.first
-      expect(member).to be_a(::Medicaid::AptcHouseholdMember)
     end
 
     it 'should not return nil for determination_reasons attribute' do
