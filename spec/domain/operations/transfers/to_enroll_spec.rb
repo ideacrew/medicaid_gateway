37f79a30b74059ab43bb2a2fde2ea2578207411a
Esme Rubinstein <erubinst@oberlin.edu>
2023-07-03 09:37:36 -0400

    adding spec

--- spec/domain/operations/transfers/to_enroll_spec.rb
+++ spec/domain/operations/transfers/to_enroll_spec.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require 'aca_entities/serializers/xml/medicaid/atp'
-require 'aca_entities/atp/transformers/cv/family'
+require "aca_entities/serializers/xml/medicaid/atp"
+require "aca_entities/atp/transformers/cv/family"
 
 describe Transfers::ToEnroll, "given a soap envelope with an valid xml payload, transfer it to Enroll" do
 
   let(:transfer_id) { "tr123" }
   let(:body) { File.read("./spec/test_data/Simple_Test_Case_L_New.xml") }
   let(:process) { described_class.new }
+  let(:operation) { Transfers::ToEnroll.new }
 
   context 'success' do
     before :each do
@@ -23,6 +24,23 @@ describe Transfers::ToEnroll, "given a soap envelope with an valid xml payload, 
 
       it 'should return success with message' do
         expect(@result.success).to eq("Transferred account to Enroll")
+      end
+    end
+  end
+
+  context 'inferred post partum period' do
+    before do
+      allow(MedicaidGatewayRegistry).to receive(:feature_enabled?).with(:infer_post_partum_period).and_return(true)
+      @inbound_transfer = create :inbound_transfer, external_id: transfer_id
+      payload = operation.send(:create_transfer, body).success
+      transformed_params = operation.send(:transform_params, payload, @inbound_transfer.id).success
+      @updated_transformed_params = operation.send(:update_inferred_defaults, transformed_params).success
+    end
+
+    context 'transformed params with nil post partum' do
+      it 'should update to false' do
+        not_pregnant_applicant = @updated_transformed_params[:family][:magi_medicaid_applications][0][:applicants].first
+        expect(not_pregnant_applicant[:pregnancy_information][:is_post_partum_period]).to eq(false)
       end
     end
   end
