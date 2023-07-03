2c3b02a8d8b5bfda6fbbbf465e5b9f73ea4b87bd
Esme Rubinstein <erubinst@oberlin.edu>
2023-07-03 09:42:41 -0400

    moving part of spec to let statements

--- spec/domain/operations/transfers/to_enroll_spec.rb
+++ spec/domain/operations/transfers/to_enroll_spec.rb
@@ -9,12 +9,11 @@ describe Transfers::ToEnroll, "given a soap envelope with an valid xml payload, 
   let(:transfer_id) { "tr123" }
   let(:body) { File.read("./spec/test_data/Simple_Test_Case_L_New.xml") }
   let(:process) { described_class.new }
-  let(:operation) { Transfers::ToEnroll.new }
+  let(:inbound_transfer) {create :inbound_transfer, external_id: transfer_id}
 
   context 'success' do
     before :each do
-      @inbound_transfer = create :inbound_transfer, external_id: transfer_id
-      @result = process.call(body, @inbound_transfer.id)
+      @result = process.call(body, inbound_transfer.id)
     end
 
     context 'with valid application' do
@@ -28,18 +27,19 @@ describe Transfers::ToEnroll, "given a soap envelope with an valid xml payload, 
     end
   end
 
+  let(:operation) { Transfers::ToEnroll.new }
+  let(:payload) {operation.send(:create_transfer, body).success}
+  let(:transformed_params) {operation.send(:transform_params, payload, inbound_transfer.id).success}
+  let(:updated_transformed_params) {operation.send(:update_inferred_defaults, transformed_params).success}
+
   context 'inferred post partum period' do
     before do
       allow(MedicaidGatewayRegistry).to receive(:feature_enabled?).with(:infer_post_partum_period).and_return(true)
-      @inbound_transfer = create :inbound_transfer, external_id: transfer_id
-      payload = operation.send(:create_transfer, body).success
-      transformed_params = operation.send(:transform_params, payload, @inbound_transfer.id).success
-      @updated_transformed_params = operation.send(:update_inferred_defaults, transformed_params).success
     end
 
     context 'transformed params with nil post partum' do
       it 'should update to false' do
-        not_pregnant_applicant = @updated_transformed_params[:family][:magi_medicaid_applications][0][:applicants].first
+        not_pregnant_applicant = updated_transformed_params[:family][:magi_medicaid_applications][0][:applicants].first
         expect(not_pregnant_applicant[:pregnancy_information][:is_post_partum_period]).to eq(false)
       end
     end
