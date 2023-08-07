916591ad066cbe9ed6675934378d46b1fc4ed04c
HARIDHAR YAMJALA <hariym471@gmail.com>
2023-08-07 14:43:49 -0400

    Update subject to person instead of application

--- spec/factories/transmittable/aces_applications.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :aces_application, class: "::Transmittable::AcesApplication" do
-    application_id {"test_application_123"}
-  end
-end
