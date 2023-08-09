bbca8423c3c4bfa810caf450d71e37f8153afd5e
Michael Karamanov <mdkaramanov@gmail.com>
2023-08-09 10:00:43 -0400

    update dependent filing thresholds for 2024 (#360)
    * update dependent filing thresholds for 2024
    
    * add missing comma

--- app/models/types.rb
+++ app/models/types.rb
@@ -113,11 +113,17 @@ module Types
         unearned_income: BigDecimal('1_100')
       }
     },
-    # adding 2024 values as placeholders to placate specs. These values are not verified.
     {
       2024 => {
-        earned_income: BigDecimal('12_600'),
-        unearned_income: BigDecimal('1_100')
+        earned_income: BigDecimal('12_950'),
+        unearned_income: BigDecimal('1_150')
+      }
+    },
+    # adding 2025 values as placeholders to placate specs. These values are not verified.
+    {
+      2025 => {
+        earned_income: BigDecimal('12_950'),
+        unearned_income: BigDecimal('1_150')
       }
     }
   ].freeze
