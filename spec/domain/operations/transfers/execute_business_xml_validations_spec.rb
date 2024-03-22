ce6e1d1d279441000b6c4ce0cf93c6ac374a6391
Michael Karamanov <michael.karamanov@ideacrew.com>
2024-03-22 17:39:47 -0400

    fix business validation spec

--- spec/domain/operations/transfers/execute_business_xml_validations_spec.rb
+++ spec/domain/operations/transfers/execute_business_xml_validations_spec.rb
@@ -3,6 +3,7 @@
 require "rails_helper"
 
 describe Transfers::ExecuteBusinessXmlValidations, dbclean: :after_each do
+  include Dry::Monads[:result]
 
   let(:subject) do
     Transfers::ExecuteBusinessXmlValidations.new
@@ -36,21 +37,37 @@ describe Transfers::ExecuteBusinessXmlValidations, dbclean: :after_each do
   end
 
   context "validator crashed" do
+    let(:validator_result) do
+      <<-XMLCODE
+      <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+      <svrl:schematron-output xmlns:svrl="http://purl.oclc.org/dsdl/svrl">
+      </svrl:schematron-output>
+      XMLCODE
+    end
     let(:payload) { double }
-    let(:failure_result) { Failure(:validator_crashed) }
+    let(:stubbed_validation_responses) { [proc { raise StandardError }, proc { validator_result }] }
     let(:result) { subject.call(payload) }
 
     before do
-      allow(AtpBusinessRulesValidationProxy).to receive(:run_validation).with(payload).and_raise("Validator Crashed")
+      allow(AtpBusinessRulesValidationProxy).to receive(:run_validation).with(payload).exactly(:twice) { stubbed_validation_responses.shift.call }
+      allow(AtpBusinessRulesValidationProxy).to receive(:reconnect!).and_return(true)
     end
 
     context "second attempt" do
       it "should reconnect the proxy" do
         result
-        # expect(AtpBusinessRulesValidationProxy).to receive(:reconnect!)
+        expect(AtpBusinessRulesValidationProxy).to have_received(:reconnect!)
+      end
+
+      context 'second attempt succeeds' do
+        it "should return a success" do
+          expect(result.success?).to be_truthy
+        end
       end
 
       context "second attempt fails" do
+        let(:stubbed_validation_responses) { [proc { raise StandardError }, proc { raise StandardError }] }
+
         it "should return a failure" do
           expect(result.failure).to eq(:validator_crashed)
         end
