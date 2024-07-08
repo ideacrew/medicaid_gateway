f722a59d524d5e637faaf7da950192e58635c75f
HArsHA Ellanki <h_vardhan@yahoo.com>
2024-07-08 12:54:31 -0400

    rubocop fixes and node version update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "medicaid_gateway_production"
 
-
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
